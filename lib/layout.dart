91de060a45310c8b73325c8d5c3f3285da392cf3
Mr. Phil <mr.phil1@gmx.net>
2021-10-30 16:53:23 +0200

    modify layout.dart * add a container function

--- lib/layout.dart
+++ lib/layout.dart
@@ -1,9 +1,24 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 Widget mainWidget() {
   return Row(children: [
-    Image.network('https://picsum.photos/100'),
-    Image.network('https://picsum.photos/100'),
-    Image.network('https://picsum.photos/100'),
+    container(),
+    container(),
   ]);
+}
+
+Widget container() {
+  return Expanded(
+      child: Container(
+    decoration: BoxDecoration(
+        color: Color(0x85a02525),
+        border: Border.all(
+          color: Color(0x85000000),
+          width: 5,
+        ),
+        borderRadius: BorderRadius.circular(4)),
+    child: Image.network('https://picsum.photos/100'),
+    margin: const EdgeInsets.all(4),
+  ));
 }
