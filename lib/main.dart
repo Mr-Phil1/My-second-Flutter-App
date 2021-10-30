d73019ba262ed475ae00427e52e7c7b2c65bf3b4
Mr. Phil <mr.phil1@gmx.net>
2021-10-30 14:28:50 +0200

    modify main.dart * add a ShuperHeroName Class

--- lib/main.dart
+++ lib/main.dart
@@ -1,69 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:super_hero/super_hero.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.red,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page6'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many ti33mes:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      title: 'My SuperHero Picker',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Welcome to my SuperHero Picker'),
+        ),
+        body: Center(
+          child: SuperHeroNameGenerator(),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add_box_outlined),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+class SuperHeroNameGenerator extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() => _SuperHeroName();
+}
+
+class _SuperHeroName extends State<SuperHeroNameGenerator>{
+  @override
+  Widget build(BuildContext context) {
+   final superHeroName=SuperHero.random();
+   return Text(superHeroName);
   }
 }
