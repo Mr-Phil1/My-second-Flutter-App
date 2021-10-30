568af9f7bb3a080eabf46e09d5eeb7f0247f3ed3
Mr. Phil <mr.phil1@gmx.net>
2021-10-30 14:45:27 +0200

    modify main.dart * add a SuperHeroName List

--- lib/main.dart
+++ lib/main.dart
@@ -7,27 +7,63 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'My SuperHero Picker',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to my SuperHero Picker'),
-        ),
-        body: Center(
-          child: SuperHeroNameGenerator(),
-        ),
-      ),
+      home: SuperHeroNameGenerator(),
     );
   }
 }
 
-class SuperHeroNameGenerator extends StatefulWidget{
+class SuperHeroNameGenerator extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => _SuperHeroName();
 }
 
-class _SuperHeroName extends State<SuperHeroNameGenerator>{
+class _SuperHeroName extends State<SuperHeroNameGenerator> {
+  final _SuperHeroNames = <String>[];
+  final _hearted = <String>{};
+
   @override
   Widget build(BuildContext context) {
-   final superHeroName=SuperHero.random();
-   return Text(superHeroName);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Welcome to my SuperHero Picker'),
+      ),
+      body: _buildBody(),
+    );
+  }
+
+  Widget _buildBody() {
+    return ListView.builder(
+        padding: EdgeInsets.all(16),
+        itemBuilder: (context, i) {
+          if (i.isOdd) {
+            return Divider();
+          }
+          if (i ~/ 2 >= _SuperHeroNames.length) {
+            _SuperHeroNames.addAll(_generateSuperHeroNames());
+          }
+          return _buildRow(_SuperHeroNames[i ~/ 2]);
+        });
+  }
+
+  Iterable<String> _generateSuperHeroNames() {
+    List<String> myNames = [
+      SuperHero.random(),
+      SuperHero.random(),
+      SuperHero.random(),
+      SuperHero.random(),
+      SuperHero.random(),
+      SuperHero.random(),
+      SuperHero.random(),
+      SuperHero.random(),
+      SuperHero.random(),
+      SuperHero.random()
+    ];
+    return myNames;
+  }
+
+  Widget _buildRow(String superHeroName) {
+    return ListTile(
+      title: Text(superHeroName),
+    );
   }
 }
